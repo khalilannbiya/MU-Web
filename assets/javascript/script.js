8271df849c1573d2b435c7b6b12692e26af8c8f7
Khalil-ann <syeichkhalil30@gmail.com>
2021-06-28 20:33:17 +0700

    first commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+
+	alert("Selamat Datang !! Fans MU");
+
+	const nama = prompt("Siapa Nama mu ?");
+	
+	const halo= document.getElementById('js');
+	
+	halo.innerHTML="Haloo" +" "+ nama + " " + "Selamat Datang !!";
+		
