63c207877323688fd7ee3610383f594e29820eb4
khalilannbiya <syeichkhalil@gmail.com>
2024-01-25 20:29:02 +0700

    refactor: changing the layouting appearance from the CSS side

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,10 @@
+alert("Selamat Datang !! Fans MU");
 
-	alert("Selamat Datang !! Fans MU");
+let nama = prompt("Siapa Nama mu ?");
+if (!nama) {
+  nama = "Fulan";
+}
 
-	const nama = prompt("Siapa Nama mu ?");
-	
-	const halo= document.getElementById('js');
-	
-	halo.innerHTML="Haloo" +" "+ nama + " " + "Selamat Datang !!";
-		
+const halo = document.getElementById("js");
+
+halo.innerHTML = "Haloo" + " " + nama + " " + "Selamat Datang !!";
